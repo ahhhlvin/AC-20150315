aef52eec5ee7a2ecf33a49d1f605762a1ca5423c
ahhhlvin <alvkuang@gmail.com>
2015-03-16 22:26:12 -0400

    exitTicket exercise upated

---
+++ src/nyc/c4q/ahhhlvin/exitTicket.java
@@ -0,0 +1,20 @@
+package nyc.c4q.ahhhlvin;
+
+/**
+ * Created by alvin2 on 3/16/15.
+ * Alvin Kuang
+ * C4Q Access Code 2.1
+ */
+public class exitTicket {
+    public static void main (String[] args) {
+
+        String word = "Access Code";
+        char letter = ' ';
+
+        System.out.println(word.length());
+
+        System.out.println(word.replace('c', 'n'));
+
+
+    }
+}
